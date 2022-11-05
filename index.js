166047b7a82fc1d456d3f04872b24e34829aac7e
caeberli <caeberli@mit.edu>
2022-11-04 23:58:12 -0700

    nothing major

--- index.js
+++ index.js
@@ -151,6 +151,91 @@ app.post("/createLoyaltyProgram", async (req, res) => {
   });
 });
 
+// Mirror the loyalty program publication to join the program
+app.post("/prepareJoinLoyaltyProgram", async (req, res) => {
+  const signerAddress = req.body.signerAddress;
+  const businessProfileId = req.body.businessProfileId;
+  const handle = req.body.handle;
+  const fullhandle = handle + ".test";
+
+  let profileId = await lensMainContract.getProfileIdByHandle(fullhandle, {
+    gasPrice: Math.round(Number(feeData.gasPrice) * 1.5),
+    gasLimit: 500000,
+  });
+
+  console.log(profileId);
+
+  const txnData = Web3EthAbi.encodeFunctionCall(
+    {
+      inputs: [
+        {
+          components: [
+            { internalType: "uint256", name: "profileId", type: "uint256" },
+            {
+              internalType: "uint256",
+              name: "profileIdPointed",
+              type: "uint256",
+            },
+            { internalType: "uint256", name: "pubIdPointed", type: "uint256" },
+            {
+              internalType: "bytes",
+              name: "referenceModuleData",
+              type: "bytes",
+            },
+            {
+              internalType: "address",
+              name: "referenceModule",
+              type: "address",
+            },
+            {
+              internalType: "bytes",
+              name: "referenceModuleInitData",
+              type: "bytes",
+            },
+          ],
+          internalType: "struct DataTypes.MirrorData",
+          name: "vars",
+          type: "tuple",
+        },
+      ],
+      name: "mirror",
+      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+
+    [
+      {
+        profileId: profileId,
+        profileIdPointed: businessProfileId,
+        pubIdPointed: 1,
+        referenceModuleData: "0x",
+        referenceModule: "0x0000000000000000000000000000000000000000",
+        referenceModuleInitData: "0x",
+      },
+    ]
+  );
+
+  console.log(txnData);
+
+  // From Metamask Documentation
+  const transactionParameters = {
+    nonce: "0x00", // ignored by MetaMask
+    gasPrice: "0xEE6B2800", // customizable by user during MetaMask confirmation.
+    gas: "0x186A0", // customizable by user during MetaMask confirmation.
+    to: "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82", // Required except during contract publications.
+    from: signerAddress, // must match user's active address.
+    value: "0x00", // Only required to send ether to the recipient from the initiating external account.
+    data: txnData,
+    // Optional, but used for defining smart contract creation and interaction.
+    chainId: "80001", // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+  };
+
+  res.json({
+    transactionParameters,
+  });
+});
+
 app.post("/testServer", async (req, res) => {
   console.log(process.env.DUMMY_KEY);
   res.json({
