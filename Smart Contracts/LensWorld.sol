98c94132d7fff40a0cb35fbbd264191429abbb99
caeberli <caeberli@mit.edu>
2022-11-04 22:29:18 -0700

    first lens endpoints for profile crestion

---
+++ Smart Contracts/LensWorld.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+// From https://github.com/worldcoin/world-id-lens/blob/main/src/HumanCheck.sol
+// Deployed via remix
+
+pragma solidity ^0.8.10;
+
+import "@worldcoin/world-id-contracts/src/libraries/ByteHasher.sol";
+import "@worldcoin/world-id-contracts/src/interfaces/IWorldID.sol";
+
+contract HumanCheck {
+    using ByteHasher for bytes;
+
+    /// @notice Emitted when a profile is verified
+    /// @param profileId The ID of the profile getting verified
+    event ProfileVerified(uint256 indexed profileId);
+
+    /// @notice Emitted when a profile is unverified
+    /// @param profileId The ID of the profile no longer verified
+    event ProfileUnverified(uint256 indexed profileId);
+
+    /// @dev The World ID instance that will be used for verifying proofs
+    IWorldID internal immutable worldId;
+
+    /// @dev The World ID group ID (always `1`)
+    uint256 internal immutable groupId;
+
+    /// @dev The World ID Action ID
+    uint256 internal immutable actionId;
+
+    /// @notice Whether a profile is verified
+    /// @dev This also generates an `isVerified(uint256) getter
+    mapping(uint256 => bool) public isVerified;
+
+    /// @dev Connection between nullifiers and profiles. Used to correctly unverify the past profile when re-verifying.
+    mapping(uint256 => uint256) internal nullifierHashes;
+
+    /// @param _worldId The WorldID instance that will verify the proofs
+    /// @param _groupId The WorldID group that contains our users (always `1`)
+    /// @param _actionId The WorldID Action ID for the proofs
+    constructor(
+        IWorldID _worldId,
+        uint256 _groupId,
+        string memory _actionId
+    ) payable {
+        worldId = _worldId;
+        groupId = _groupId;
+        actionId = abi.encodePacked(_actionId).hashToField();
+    }
+
+    /// @notice Verify a Lens profile
+    /// @param profileId The Lens profile you want to verify
+    /// @param root The root of the Merkle tree (returned by the JS SDK).
+    /// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
+    /// @param proof The zero-knowledge proof that demostrates the claimer is registered with World ID (returned by the JS widget).
+    function verify(
+        uint256 profileId,
+        uint256 root,
+        uint256 nullifierHash,
+        uint256[8] calldata proof
+    ) public payable {
+        worldId.verifyProof(
+            root,
+            groupId,
+            abi.encodePacked(profileId).hashToField(),
+            nullifierHash,
+            actionId,
+            proof
+        );
+
+        if (nullifierHashes[nullifierHash] != 0) {
+            uint256 prevProfileId = nullifierHashes[nullifierHash];
+
+            isVerified[prevProfileId] = false;
+            emit ProfileUnverified(prevProfileId);
+        }
+
+        isVerified[profileId] = true;
+        nullifierHashes[nullifierHash] = profileId;
+
+        emit ProfileVerified(profileId);
+    }
+}
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ import axios from "axios";
 import alc from "alchemy-sdk";
 import Web3 from "web3";
 import Web3EthAbi from "web3-eth-abi";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Getting all ABIs
+import lensProfileABI from "./ABIs/lensProfileABI.json" assert { type: "json" };
+import lensMainABI from "./ABIs/lensMainABI.json" assert { type: "json" };
 
 // Initializing Express
 const app = express();
@@ -19,10 +25,103 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/testServer", async (req, res) => {
+// Our Ethers Signer with Alchemy Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(
+  "maticmum",
+  process.env.ALCHEMY_API_KEY
+);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, alchemyProvider);
+const feeData = await alchemyProvider.getFeeData();
+
+//////////////////////////////// LENS ENDPOINTS AND LOGIC
+
+const mockProfileContract = new ethers.Contract(
+  "0x420f0257D43145bb002E69B14FF2Eb9630Fc4736",
+  lensProfileABI,
+  signer
+);
+
+const lensMainContract = new ethers.Contract(
+  "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82",
+  lensMainABI,
+  signer
+);
+
+// We create the profiles with out key
+app.post("/lensCreateProfile", async (req, res) => {
+  const address = req.body.address;
+  const handle = req.body.handle;
+
+  let tx = await mockProfileContract.proxyCreateProfile(
+    {
+      to: address,
+      handle: handle,
+      imageURI: "",
+      followModule: "0x0000000000000000000000000000000000000000",
+      followModuleInitData: "0x",
+      followNFTURI: "",
+    },
+    {
+      gasPrice: Math.round(Number(feeData.gasPrice) * 1.5),
+      gasLimit: 500000,
+    }
+  );
+  //console.log(tx);
   res.json({
-    error: "false",
     tx,
+  });
+});
+
+app.post("/prepareDispatchTx", async (req, res) => {
+  const signerAddress = req.body.signerAddress;
+  const handle = req.body.handle;
+  const fullhandle = handle + ".test";
+
+  let profileId = await lensMainContract.getProfileIdByHandle(fullhandle, {
+    gasPrice: Math.round(Number(feeData.gasPrice) * 1.5),
+    gasLimit: 500000,
+  });
+
+  console.log(profileId);
+
+  const txnData = Web3EthAbi.encodeFunctionCall(
+    {
+      inputs: [
+        { internalType: "uint256", name: "profileId", type: "uint256" },
+        { internalType: "address", name: "dispatcher", type: "address" },
+      ],
+      name: "setDispatcher",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    [profileId, "0x0687CB24af3884Cb221E5B27cC459EF880F24228"]
+  );
+
+  console.log(txnData);
+
+  // From Metamask Documentation
+  const transactionParameters = {
+    nonce: "0x00", // ignored by MetaMask
+    gasPrice: "0x09184e72a000", // customizable by user during MetaMask confirmation.
+    gas: "0x2710", // customizable by user during MetaMask confirmation.
+    to: "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82", // Required except during contract publications.
+    from: signerAddress, // must match user's active address.
+    value: "0x00", // Only required to send ether to the recipient from the initiating external account.
+    data: txnData,
+    // Optional, but used for defining smart contract creation and interaction.
+    chainId: "80001", // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+  };
+
+  res.json({
+    transactionParameters,
+  });
+});
+
+app.post("/testServer", async (req, res) => {
+  console.log(process.env.DUMMY_KEY);
+  res.json({
+    error: false,
   });
 });
 
